02883a8be0710f22a97f7ac71788ed1b17867f1f
muhaeminabdullah <muhaeminabdul235@gmail.com>
2018-08-26 11:58:18 +0700

    v-01

---
+++ app/src/main/java/com/muhaemin/praktek_uas/Server.java
@@ -0,0 +1,8 @@
+package com.muhaemin.praktek_uas;
+/**
+ * Created by Angga Hendrawan on 13/08/2018.
+ */
+
+public class Server {
+    public static final String URL = "http://sasmitoh.nitarahmawati.my.id/";
+}
